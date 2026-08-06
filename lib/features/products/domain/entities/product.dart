c227f897d1739185e84358526ae41b57cd684089
noshadramdani <noshadprofessional69@gmail.com>
2026-08-06 16:58:50 +0500

    feat: add Product domain entity

---
+++ lib/features/products/domain/entities/product.dart
@@ -0,0 +1,19 @@
+class Product {
+  final int id;
+  final String title;
+  final String description;
+  final double price;
+  final String category;
+  final String thumbnail;
+  final int stock;
+
+  const Product({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.category,
+    required this.thumbnail,
+    required this.stock,
+  });
+}
