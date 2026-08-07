a68822658909a2d8dc849e9e7fd65c118b1dbc96
noshadramdani <noshadprofessional69@gmail.com>
2026-08-07 12:57:36 +0500

    feat: implement StatefulShellRoute with bottom nav and cart badge

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,18 +1,89 @@
+import 'package:ecommerce_app/features/cart/presentation/providers/cart_provider.dart';
+import 'package:ecommerce_app/features/cart/presentation/screens/cart_screen.dart';
 import 'package:ecommerce_app/features/products/domain/entities/product.dart';
-import 'package:ecommerce_app/features/products/presentation/screens/product_detail_scree.dart';
+import 'package:ecommerce_app/features/products/presentation/screens/product_detail_screen.dart';
 import 'package:ecommerce_app/features/products/presentation/screens/product_list_screen.dart';
+import 'package:ecommerce_app/features/profile/presentation/screens/profile_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 final GoRouter appRouter = GoRouter(
   initialLocation: '/',
   routes: [
-    GoRoute(path: '/', builder: (context, state) => const ProductListScreen()),
     GoRoute(
       path: '/product/:id',
       builder: (context, state) {
         final product = state.extra as Product;
         return ProductDetailScreen(product: product);
       },
+    ),
+
+    StatefulShellRoute.indexedStack(
+      builder: (context, state, navigationShell) {
+        return Consumer(
+          builder: (context, ref, _) {
+            final cartCount = ref.watch(
+              cartProvider.select(
+                (items) => items.fold(0, (sum, item) => sum + item.quantity),
+              ),
+            );
+
+            return Scaffold(
+              body: navigationShell,
+              bottomNavigationBar: BottomNavigationBar(
+                currentIndex: navigationShell.currentIndex,
+                onTap: (index) => navigationShell.goBranch(index),
+                items: [
+                  const BottomNavigationBarItem(
+                    icon: Icon(Icons.home),
+                    label: 'Home',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Badge(
+                      label: Text('$cartCount'),
+                      isLabelVisible: cartCount > 0,
+                      child: const Icon(Icons.shopping_cart),
+                    ),
+                    label: 'Cart',
+                  ),
+                  const BottomNavigationBarItem(
+                    icon: Icon(Icons.person),
+                    label: 'Profile',
+                  ),
+                ],
+              ),
+            );
+          },
+        );
+      },
+
+      branches: [
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: '/',
+              builder: (context, state) => const ProductListScreen(),
+            ),
+          ],
+        ),
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: '/cart',
+              builder: (context, state) => const CartScreen(),
+            ),
+          ],
+        ),
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: '/profile',
+              builder: (context, state) => const ProfileScreen(),
+            ),
+          ],
+        ),
+      ],
     ),
   ],
 );
