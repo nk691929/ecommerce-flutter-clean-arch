6e48084f2a0245c7408e3871f3b316bcca58167d
noshadramdani <noshadprofessional69@gmail.com>
2026-08-07 10:04:19 +0500

    refactor: switch MaterialApp to MaterialApp.router for go_router integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
-import 'package:ecommerce_app/features/products/presentation/screens/product_list_screen.dart';
+import 'package:ecommerce_app/core/router/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(
-    ProviderScope(
-      child: MaterialApp(
-        home: ProductListScreen(),
-      ),
-    ),
-  );
+  runApp(ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(routerConfig: appRouter);
+  }
 }
