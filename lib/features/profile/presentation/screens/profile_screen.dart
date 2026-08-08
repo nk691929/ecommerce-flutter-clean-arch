07fb72469f694ed6d73eaf24a24c877b63f6964e
noshadramdani <noshadprofessional69@gmail.com>
2026-08-08 08:54:42 +0500

    feat: add dark mode toggle to profile screen

--- lib/features/profile/presentation/screens/profile_screen.dart
+++ lib/features/profile/presentation/screens/profile_screen.dart
@@ -1,13 +1,21 @@
+import 'package:ecommerce_app/core/theme/theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends ConsumerWidget {
   const ProfileScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context,WidgetRef ref) {
+    final themeMode=ref.watch(themeProvider);
     return Scaffold(
       appBar: AppBar(title: const Text('Profile')),
-      body: const Center(child: Text('Profile Screen')),
+      body: Center(
+        child:SwitchListTile(
+          title: Text('Dark Mode'),
+          value: themeMode==ThemeMode.dark, 
+          onChanged: (_)=>ref.read(themeProvider.notifier).toggle())
+          ),
     );
   }
 }
