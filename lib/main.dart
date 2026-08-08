9db15c346b07f4efc821831b795e89cedf89484d
noshadramdani <noshadprofessional69@gmail.com>
2026-08-08 08:54:42 +0500

    feat: wire theme provider into MaterialApp.router

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_app/core/router/app_router.dart';
+import 'package:ecommerce_app/core/theme/theme_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -6,11 +7,17 @@ void main() {
   runApp(ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp.router(routerConfig: appRouter);
+  Widget build(BuildContext context, WidgetRef ref) {
+    final themeMode = ref.watch(themeProvider);
+    return MaterialApp.router(
+      routerConfig: appRouter,
+      themeMode: themeMode,
+      theme: ThemeData.light(useMaterial3: true),
+      darkTheme: ThemeData.dark(useMaterial3: true),
+    );
   }
 }
